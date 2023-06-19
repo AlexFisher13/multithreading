251b1b50060a944a29424985fb00ac9d9db51d99
fisher <fisheralexandr@gmail.com>
2023-06-20 00:52:41 +0300

    atomic, synchronized

---
+++ src/main/java/org/example/Atomic.java
@@ -0,0 +1,33 @@
+package org.example;
+
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Пример показывает, что атомарные типы делают операцию инкремента атомарной и не кэшируемой
+ * и потоки не будут перезаписывать друг друга.
+ * Программа работает медленнее, но гарантированно выдает правильный результат.
+ */
+
+public class Atomic {
+    public static final int N = 10_000_000;
+//    public static volatile int counter = 0;
+    public static AtomicInteger counter = new AtomicInteger(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t0 = new Thread(() -> {
+            for (int i = 0; i < N; i++) counter.incrementAndGet();
+        });
+        t0.start();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < N; i++) counter.incrementAndGet();
+        });
+        t1.start();
+
+        t0.join();  // дожидаемся до конца выполнения потоков
+        t1.join();  // иначе counter будет совсем чуть-чуть
+
+        System.out.println(counter);
+    }
+}
