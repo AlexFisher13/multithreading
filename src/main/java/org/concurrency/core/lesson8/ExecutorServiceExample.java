da9f718e29ea1ae2d18ef2345346e3cd697a44fa
fisher <fisheralexandr@gmail.com>
2023-07-16 17:29:51 +0300

    ExecutorService

---
+++ src/main/java/org/concurrency/core/lesson8/ExecutorServiceExample.java
@@ -0,0 +1,29 @@
+package org.concurrency.core.lesson8;
+
+import java.util.concurrent.*;
+
+/**
+ * В этом примере мы видим создание пула и наполнение его 100 задачами.
+ * Пробуем создать single, cached и fixed threadPool.
+ * Судя по выводу в консоль cachedThreadPool решил создать 100 потоков, под 100 задач.
+ */
+public class ExecutorServiceExample {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+//        ExecutorService executorService = Executors.newFixedThreadPool(5);
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        for (int i = 0; i < 100; i++) {
+            int taskNumber = i;
+            executorService.submit(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } catch (InterruptedException ignored) {}
+                System.out.println(Thread.currentThread().getName() + " : Task " + taskNumber);
+            });
+        }
+
+        executorService.shutdown();
+
+    }
+}
---
+++ src/main/java/org/concurrency/core/lesson8/ExecutorServiceExample2.java
@@ -0,0 +1,38 @@
+package org.concurrency.core.lesson8;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * В этом примере мы рассмотрим метод shutdownNow,
+ * который немедленно завершает работу пула потоков и возвращает лист невыполенных задач.
+ */
+public class ExecutorServiceExample2 {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        for (int i = 0; i < 100; i++) {
+            int taskNumber = i;
+            executorService.submit(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    System.out.println("Task is interrupted");
+                }
+                System.out.println(Thread.currentThread().getName() + " : Task " + taskNumber);
+            });
+        }
+
+        List<Runnable> runnables = executorService.shutdownNow();
+        System.out.println("Count of not finished tasks: " + runnables.size());
+
+        ExecutorService executorService2 = Executors.newFixedThreadPool(5);
+        for (Runnable task: runnables) {
+            executorService2.submit(task);
+        }
+
+        executorService2.shutdown();
+    }
+}
